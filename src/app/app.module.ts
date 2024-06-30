093c91860d203ba635314d801fea0d18ce7ff1ea
AbdelrahmanGebaly <abdoelgebaly2572000@gmail.com>
2024-06-30 15:26:36 +0300

    upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TransferComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
