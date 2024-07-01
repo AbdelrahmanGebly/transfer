af3e82be7e87c89a29b8b1af032c81557c93ed24
AbdelrahmanGebaly <abdoelgebaly2572000@gmail.com>
2024-07-01 11:41:35 +0300

    transferWithCss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TransferComponent } from './components/transfer/transfer.component';
+import { AgainTransferWithCssComponent } from './components/again-transfer-with-css/again-transfer-with-css.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TransferComponent
+    TransferComponent,
+    AgainTransferWithCssComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/again-transfer-with-css/again-transfer-with-css.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-again-transfer-with-css',
+  templateUrl: './again-transfer-with-css.component.html',
+  styleUrls: ['./again-transfer-with-css.component.scss']
+})
+export class AgainTransferWithCssComponent {
+
+}
