db71a9f766f5ecbebc4ef4bc314793f1357536c5
AbdelrahmanGebaly <abdoelgebaly2572000@gmail.com>
2024-07-03 17:36:46 +0300

    add validations

--- src/app/components/exchange-goods/exchange-goods.component.ts
+++ src/app/components/exchange-goods/exchange-goods.component.ts
@@ -6,17 +6,23 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./exchange-goods.component.scss']
 })
 export class ExchangeGoodsComponent {
-  myForm!:FormGroup;
 
-
-
-
-
-
-
-
-
+  myForm:FormGroup = new FormGroup({
+    branch:new FormControl(''),
+    username:new FormControl(''),
+    time:new FormControl(''),
+    fileNumber:new FormControl(''),
+    fileDate: new FormControl(''),
+    endRequestDate:new FormControl(''),
+    adoptedBy:new FormControl(''),
+    recommendedBy:new FormControl(''),
+    project:new FormControl(''),
+    location:new FormControl(''),
+    storage:new FormControl(''),
+    file:new FormControl(''),
+    notes : new FormControl(''),
+  })
   handleForm(){
-    
+    console.log(this.myForm.value)
   }
 }
